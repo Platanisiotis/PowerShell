9a70a408385cb931d4c429ed104ee839a3e704f7
Alexander Platanisiotis <alexander@platanisiotis.com>
2016-12-18 15:56:02 +1100

    Modified general spacing. No specifc code changes

--- Clear-WSUSDatabase.ps1
+++ Clear-WSUSDatabase.ps1
@@ -4,9 +4,9 @@
     $wsus = [Microsoft.UpdateServices.Administration.AdminProxy]::GetUpdateServer(); 
     $cleanupScope = new-object Microsoft.UpdateServices.Administration.CleanupScope; 
     $cleanupScope.DeclineSupersededUpdates = $true        
-    $cleanupScope.DeclineExpiredUpdates         = $true 
-    $cleanupScope.CleanupObsoleteUpdates     = $true 
-    $cleanupScope.CompressUpdates                  = $true 
+    $cleanupScope.DeclineExpiredUpdates = $true 
+    $cleanupScope.CleanupObsoleteUpdates = $true 
+    $cleanupScope.CompressUpdates = $true 
     #$cleanupScope.CleanupObsoleteComputers = $true 
     $cleanupScope.CleanupUnneededContentFiles = $true 
     $cleanupManager = $wsus.GetCleanupManager(); 
