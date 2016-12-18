b774b66ec39db1a75279a9207ec89a8ef2549d30
Alexander Platanisiotis <alexander@platanisiotis.com>
2016-12-18 15:54:59 +1100

    Initial commit

---
+++ Clear-WSUSDatabase.ps1
@@ -0,0 +1,14 @@
+﻿Function Clear-WSUSDatabase 
+{
+    [reflection.assembly]::LoadWithPartialName("Microsoft.UpdateServices.Administration") | out-null 
+    $wsus = [Microsoft.UpdateServices.Administration.AdminProxy]::GetUpdateServer(); 
+    $cleanupScope = new-object Microsoft.UpdateServices.Administration.CleanupScope; 
+    $cleanupScope.DeclineSupersededUpdates = $true        
+    $cleanupScope.DeclineExpiredUpdates         = $true 
+    $cleanupScope.CleanupObsoleteUpdates     = $true 
+    $cleanupScope.CompressUpdates                  = $true 
+    #$cleanupScope.CleanupObsoleteComputers = $true 
+    $cleanupScope.CleanupUnneededContentFiles = $true 
+    $cleanupManager = $wsus.GetCleanupManager(); 
+    $cleanupManager.PerformCleanup($cleanupScope); 
+}
