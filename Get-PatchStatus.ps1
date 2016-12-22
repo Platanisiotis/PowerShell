1042ee293b0e3ecf48fcd5696c3bd3b254240a89
Alexander Platanisiotis <alexander@platanisiotis.com>
2016-12-22 21:11:47 +1100

    Initial commit

---
+++ Get-PatchStatus.ps1
@@ -0,0 +1,26 @@
+﻿Function Get-PatchStatus
+{
+    [cmdletBinding(ConfirmImpact='Medium')]
+    [OutputType([String])]
+    Param ()
+    Begin 
+    {}
+    Process
+    {
+        # Creating an Update Session Object and storing it in $UpdateSession
+        $UpdateSession  = New-Object -ComObject Microsoft.Update.Session
+
+        # Running the methods to query the updates required
+        $UpdateSearcher = $UpdateSession.CreateUpdateSearcher()
+        $SearchResult   = $UpdateSearcher.Search("IsAssigned=1 and IsHidden=0 and IsInstalled=0")
+        $Critical       = $SearchResult.updates | Where-Object { $_.MsrcSeverity -eq "Critical" }
+        $important      = $SearchResult.updates | Where-Object { $_.MsrcSeverity -eq "Important" }
+        $other          = $SearchResult.updates | Where-Object { $_.MsrcSeverity -eq $null }
+        Write-Verbose "Total     = $($SearchResult.updates.count)"
+        Write-Verbose "Critical  = $($Critical.count)"
+        Write-Verbose "Important = $($Important.count)"
+        Write-Verbose "Other     = $($other.count)"
+    }
+    End 
+    {}
+}
