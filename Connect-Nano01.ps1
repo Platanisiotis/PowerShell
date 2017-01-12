60f5a7c056f4b4badb53cf709a1eb2f0fbcac38c
Alexander Platanisiotis <alexander@platanisiotis.com>
2017-01-12 12:47:02 +1100

    initial commit

---
+++ Connect-Nano01.ps1
@@ -0,0 +1,8 @@
+﻿#requires -runasadministrator
+Function Connect-Nano01 {
+    $ServerName = "nano01.platanisiotis.com"
+    Start-Service WinRM -Confirm:$false
+    Set-Item WSMan:\localhost\Client\TrustedHosts -Value $ServerName
+    $Credential = Get-Credential
+    Enter-PSSession -ComputerName $ServerName -Credential $Credential -Verbose
+}
