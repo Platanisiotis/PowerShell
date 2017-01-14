e02a1d1bfd73e9bed231731d3fcfbe0b6c716dd5
Platanisiotis <alexander@platanisiotis.com>
2017-01-14 18:30:24 +1100

    Added :$true

--- runbooks/TML-YammerCasualClothingNotification.ps1
+++ runbooks/TML-YammerCasualClothingNotification.ps1
@@ -9,9 +9,9 @@ If ($x -ge "30")
         -Subject "Tomorrow will be between $y - $x degrees, so be sure to wear casual clothing and bring a spare set of business clothes if you will be seeing a client. The temperature will be checked every afternoon at 5PM. AP Bot" `
         -SmtpServer "smtp.office365.com" `
         -Credential $Credential `
-        -UseSsl `
+        -UseSsl:$true `
         -Port "587" `
         -Body "/AP Bot" `
         -From "svc_Email_Script_Account@themissinglink.com.au" `
-        -BodyAsHtml
+        -BodyAsHtml:$true
 }
