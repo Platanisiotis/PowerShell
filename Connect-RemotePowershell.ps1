cbd7ee7f36f80ae3b6d4e34eb69bf1c7e7438307
Alexander Platanisiotis <alexander@platanisiotis.com>
2017-01-14 18:54:54 +1100

    replacing connect-nano1 with a generic function

---
+++ Connect-RemotePowershell.ps1
@@ -0,0 +1,15 @@
+﻿#requires -runasadministrator
+Function Connect-RemotePowershell($ServerName) 
+{
+    if ((Get-Service winrm).Status -ne "Running")
+    {
+        Start-Service WinRM -Confirm:$false
+    }
+    $curValue = (get-item wsman:\localhost\Client\TrustedHosts).value
+    if ($curValue -notlike $ServerName)
+    {
+        Set-Item WSMan:\localhost\Client\TrustedHosts -Value "$curValue, $ServerName"
+    }
+    $Credential = Get-Credential
+    Enter-PSSession -ComputerName $ServerName -Credential $Credential -Verbose
+}
