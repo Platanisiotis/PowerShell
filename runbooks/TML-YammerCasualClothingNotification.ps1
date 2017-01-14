2bcff2ad960a5dc26a2ba50329ef4c40493b68c3
Platanisiotis <alexander@platanisiotis.com>
2017-01-14 18:22:05 +1100

    Updated TML-YammerCasualClothingNotification.ps1 in Azure Automation.

--- runbooks/TML-YammerCasualClothingNotification.ps1
+++ runbooks/TML-YammerCasualClothingNotification.ps1
@@ -2,13 +2,13 @@
 [int]$y = ([xml]$(wget ftp://ftp.bom.gov.au/anon/gen/fwo/IDN11060.xml -usebasicparsing)).product.forecast.area[2].("forecast-period")[1].element[2].("#text")
 If ($x -ge "30") 
 {
-    $userCredential = Get-AutomationPSCredential -Name 'tml reporting'
+    $Credential = Get-AutomationPSCredential -Name 'tml reporting'
     Send-MailMessage `
         -To "9894045+themissinglink.com.au@yammer.com" `
         -Cc "aplatanisiotis@themissinglink.com.au" `
         -Subject "Tomorrow will be between $y - $x degrees, so be sure to wear casual clothing and bring a spare set of business clothes if you will be seeing a client. The temperature will be checked every afternoon at 5PM. AP Bot" `
         -SmtpServer "smtp.office365.com" `
-        -Credential $userCredential `
+        -Credential $Credential `
         -UseSsl `
         -Port "587" `
         -Body "/AP Bot" `
