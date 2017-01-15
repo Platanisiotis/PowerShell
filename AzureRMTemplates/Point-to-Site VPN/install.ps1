1afd1a60a76579c8710de88ec0338bec9bb97c07
Alexander Platanisiotis <alexander@platanisiotis.com>
2017-01-15 11:50:25 +1100

    json file swap

--- AzureRMTemplates/Point-to-Site VPN/install.ps1
+++
@@ -1,7 +0,0 @@
-New-AzureRmResourceGroup -Name "VPN" -Location "australiaeast" -Verbose
-New-AzureRmResourceGroupDeployment `
-    -Name "Vvpndeployment" `
-    -ResourceGroupName "VPN" `
-    -TemplateUri https://github.com/Platanisiotis/PowerShell/blob/master/AzureRMTemplates/Point-to-Site%20VPN/azuredeploy.json `
-    -TemplateParameterFile https://github.com/Platanisiotis/PowerShell/blob/master/AzureRMTemplates/Point-to-Site%20VPN/azuredeploy.parameters.json `
-    -Verbose
