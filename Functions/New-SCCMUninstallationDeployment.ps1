b28d1967e92f84f3bd9b1d9f6c733a5d03a10ca5
Platanisiotis <alexander@platanisiotis.com>
2017-01-16 12:04:05 +1100

    Update New-SCCMUninstallationDeployment.ps1

--- Functions/New-SCCMUninstallationDeployment.ps1
+++ Functions/New-SCCMUninstallationDeployment.ps1
@@ -1,14 +1,13 @@
 Function New-SCCMUninstallationDeployment {  
-    Param([ValidateSet("false","true")]$Verbose = "false",  
-          $ApplicationName = $(Read-Host)  
-          )  
-    sl "D:\ConfigMgr\Microsoft Configuration Manager\AdminConsole\bin"                                                                                                                    
+    Param($ApplicationName = $(Read-Host)
+    )  
+    Set-Location "D:\ConfigMgr\Microsoft Configuration Manager\AdminConsole\bin"                                                                                                                    
     Import-Module ".\ConfigurationManager.psd1"  
-    sl $(((Get-PSDrive | ? {$_.Provider -like "AdminUI.PS.Provider\CMSite"})[0].name)+":")  
+    Set-Location $(((Get-PSDrive | ? {$_.Provider -like "AdminUI.PS.Provider\CMSite"})[0].name)+":")  
     New-CMDeviceCollection `  
         -Name "Uninstall $ApplicationName" `  
         -LimitingCollectionName "All Systems" `  
-        -Verbose:$true 
+        -Verbose:$true
     Add-CMDeviceCollectionQueryMembershipRule `  
         -CollectionName "Uninstall $ApplicationName" `  
         -QueryExpression "select SMS_R_System.NetbiosName, SMS_G_System_ADD_REMOVE_PROGRAMS.DisplayName from SMS_R_System inner join SMS_G_System_ADD_REMOVE_PROGRAMS on SMS_G_System_ADD_REMOVE_PROGRAMS.ResourceId = SMS_R_System.ResourceId where SMS_G_System_ADD_REMOVE_PROGRAMS.DisplayName like '%$ApplicationName%'" `  
