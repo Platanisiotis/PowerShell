2b68e3c7d74da8a881b560e3f9ae475dcca74ecd
Platanisiotis <alexander@platanisiotis.com>
2017-02-04 14:35:59 +1100

    Updated from email to API

--- runbooks/TML-YammerCasualClothingNotification.ps1
+++ runbooks/TML-YammerCasualClothingNotification.ps1
@@ -1,17 +1,16 @@
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
 [int]$x = ([xml]$(wget ftp://ftp.bom.gov.au/anon/gen/fwo/IDN11060.xml -usebasicparsing)).product.forecast.area[2].("forecast-period")[1].element[3].("#text")
 [int]$y = ([xml]$(wget ftp://ftp.bom.gov.au/anon/gen/fwo/IDN11060.xml -usebasicparsing)).product.forecast.area[2].("forecast-period")[1].element[2].("#text")
-If ($x -ge "30") 
+If ($x -ge "10") 
 {
-    $Credential = Get-AutomationPSCredential -Name 'tml reporting'
-    Send-MailMessage `
-        -To "9894045+themissinglink.com.au@yammer.com" `
-        -Cc "aplatanisiotis@themissinglink.com.au" `
-        -Subject "Tomorrow will be between $y - $x degrees, so be sure to wear casual clothing and bring a spare set of business clothes if you will be seeing a client. The temperature will be checked every afternoon at 5PM. AP Bot" `
-        -SmtpServer "smtp.office365.com" `
-        -Credential $Credential `
-        -UseSsl:$true `
-        -Port "587" `
-        -Body "/AP Bot" `
-        -From "svc_Email_Script_Account@themissinglink.com.au" `
-        -BodyAsHtml:$true
+    $requestBody = @{}
+    $requestBody.body = "Tomorrow will be between $y - $x degrees, so be sure to wear casual clothing and bring a spare set of business clothes if you will be seeing a client.
+    
+    The temperature will be checked every afternoon at 5PM."
+    $requestBody.group_id = "10567926"
+    $requestBody = ConvertTo-Json $requestBody
+    $headers = New-Object 'System.Collections.Generic.Dictionary[[String],[String]]'
+    $headers.Add('Authorization', 'Bearer ' + "474903-mCWG5dY3DkC5VwAwEvb0Kw")
+    $target = 'https://www.yammer.com/api/v1/messages.json'
+    $response = Invoke-RestMethod $target -Headers $headers -Method POST -Body $requestBody -ContentType application/json
 }
