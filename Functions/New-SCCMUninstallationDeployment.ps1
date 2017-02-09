d8677ff0aba1c204e80837d214824b862b7df4aa
Platanisiotis <alexander@platanisiotis.com>
2017-02-09 15:57:12 +1100

    Update New-SCCMUninstallationDeployment.ps1

--- Functions/New-SCCMUninstallationDeployment.ps1
+++ Functions/New-SCCMUninstallationDeployment.ps1
@@ -1,23 +1,24 @@
-Function New-SCCMUninstallationDeployment {  
+Function New-CMUninstallationDeployment 
+{  
     Param($ApplicationName = $(Read-Host)
     )  
     Set-Location "D:\ConfigMgr\Microsoft Configuration Manager\AdminConsole\bin"                                                                                                                    
     Import-Module ".\ConfigurationManager.psd1"  
     Set-Location $(((Get-PSDrive | ? {$_.Provider -like "AdminUI.PS.Provider\CMSite"})[0].name)+":")  
-    New-CMDeviceCollection `  
-        -Name "Uninstall $ApplicationName" `  
-        -LimitingCollectionName "All Systems" `  
+    New-CMDeviceCollection `
+        -Name "Uninstall $ApplicationName" `
+        -LimitingCollectionName "All Systems" `
         -Verbose:$true
-    Add-CMDeviceCollectionQueryMembershipRule `  
-        -CollectionName "Uninstall $ApplicationName" `  
-        -QueryExpression "select SMS_R_System.NetbiosName, SMS_G_System_ADD_REMOVE_PROGRAMS.DisplayName from SMS_R_System inner join SMS_G_System_ADD_REMOVE_PROGRAMS on SMS_G_System_ADD_REMOVE_PROGRAMS.ResourceId = SMS_R_System.ResourceId where SMS_G_System_ADD_REMOVE_PROGRAMS.DisplayName like '%$ApplicationName%'" `  
-        -RuleName "$ApplicationName Installed" `  
+    Add-CMDeviceCollectionQueryMembershipRule `
+        -CollectionName "Uninstall $ApplicationName" `
+        -QueryExpression "select SMS_R_System.NetbiosName, SMS_G_System_ADD_REMOVE_PROGRAMS.DisplayName from SMS_R_System inner join SMS_G_System_ADD_REMOVE_PROGRAMS on SMS_G_System_ADD_REMOVE_PROGRAMS.ResourceId = SMS_R_System.ResourceId where SMS_G_System_ADD_REMOVE_PROGRAMS.DisplayName like '%$ApplicationName%'" `
+        -RuleName "$ApplicationName Installed" `
         -Verbose:$true
-    Start-CMApplicationDeployment `  
-        -CollectionName "Uninstall $ApplicationName" `  
-        -Name "$ApplicationName" `  
-        -DeployAction Uninstall `  
-        -DeployPurpose Required `  
-        -UserNotification DisplaySoftwareCenterOnly `  
+    Start-CMApplicationDeployment `
+        -CollectionName "Uninstall $ApplicationName" `
+        -Name "$ApplicationName" `
+        -DeployAction Uninstall `
+        -DeployPurpose Required `
+        -UserNotification DisplaySoftwareCenterOnly `
         -Verbose:$true 
-} 
+}
